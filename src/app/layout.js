9b14b0d15b7c3c3349e843d66287f6929c832a0c
Aryan soni <aryansoni1973@gmail.com>
2024-09-07 17:44:26 +0530

    Update SEO metadata and improve OpenGraph descriptions
    - Added "Jabalpur" to the site title for improved local SEO targeting.
    - Optimized meta description for clarity and better keyword placement, emphasizing expertise in responsive web applications and modern technologies (HTML, CSS, JavaScript, React, Next.js, Tailwind CSS).
    - Updated OpenGraph title and description to ensure consistency with SEO goals, focusing on building digital solutions.
    - Included `type: "website"` in OpenGraph metadata for more accurate content classification across platforms.
    
    This change enhances the site's visibility in both search engine results and social media previews, improving overall SEO performance.

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,23 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Aryan Soni | Frontend Developer & UI/UX Developer | Web Development Solutions",
+  title: "Aryan Soni | Frontend Developer & UI/UX Developer | Web Development Solutions | Jabalpur",
   description: "Experienced Frontend & UI/UX Developer in Jabalpur. Specializing in responsive, dynamic web applications with expertise in HTML, CSS, JavaScript, React, Next.js and Tailwind CSS. Let's innovate together!",
   icons: {
     icon: '/icons/logo.svg',
+  },
+  metadataBase: new URL("https://aryansoni.vercel.app/"),
+  openGraph: {
+    title: "Aryan Soni | Frontend & UI/UX Developer | Web Development",
+    description: "Experienced Frontend & UI/UX Developer in Jabalpur. Specializing in specializing in responsive web apps with expertise in HTML, CSS, JavaScript, React, Next.js and Tailwind CSS. Let's build digital solutions!",
+    url: "https://aryansoni.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/projects/aryansoni.png",
+        alt: "Aryan Soni | Frontend & UI/UX Developer | Web Development",
+      },
+    ],
   }
 };
 
